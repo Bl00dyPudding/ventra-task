a9c107caccdecab8b32f00684b93c9785080b7d0
Bloody Pudding <bloodypudding@gmail.com>
2022-03-10 20:11:32 +0300

    table added

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input type="text" placeholder="Количество записей">
+    <input type="text" placeholder="Записей на странице">
+    <input type="text" placeholder="Набор слов">
+    <VueTable></VueTable>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      VueTable: () => import('./components/VueTable')
+    }
   }
-}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
